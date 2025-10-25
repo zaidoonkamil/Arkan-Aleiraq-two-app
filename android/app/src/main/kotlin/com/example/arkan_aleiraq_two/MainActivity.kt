d123faf3d7cb94c9871960bd48c15662558adf70
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-10-26 00:55:22 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/arkan_aleiraq_two/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.arkan_aleiraq_two
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+import 'bloc_observer.dart';
+import 'core/network/local/cache_helper.dart';
+import 'core/network/remote/dio_helper.dart';
+import 'core/styles/themes.dart';
+import 'features/splash/splash.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  await CacheHelper.init();
+  DioHelper.init();
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.Debug.setAlertLevel(OSLogLevel.none);
+  OneSignal.initialize("b9a418d7-7d51-4c44-b538-f65a56728858");
+  OneSignal.Notifications.requestPermission(true);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeService().lightTheme,
+      home: SplashScreen(),
+    );
+  }
+}
